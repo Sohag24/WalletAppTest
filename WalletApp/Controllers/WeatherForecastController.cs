59585e3d1ef1f5f04181b46828af7f2e7341307d
mdsohagali <mdsohagali@MSA-ST-LP>
2023-02-20 09:46:02 +0600

    First Commit

---
+++ WalletApp/Controllers/WeatherForecastController.cs
@@ -0,0 +1,81 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace WalletApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        //[HttpGet(Name = "GetWeatherForecast")]
+        //public IEnumerable<WeatherForecast> Get()
+        //{
+        //    return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+        //    {
+        //        Date = DateTime.Now.AddDays(index),
+        //        TemperatureC = Random.Shared.Next(-20, 55),
+        //        Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+        //    })
+        //    .ToArray();
+        //}
+
+        [HttpPost(Name = "SendWeatherForecast")]
+        public IEnumerable<WeatherForecast> Post(int flag)
+        {
+            if (flag == 1)
+            {
+                return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+                {
+                    Date = DateTime.Now.AddDays(index),
+                    TemperatureC = Random.Shared.Next(-20, 55),
+                    Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                })
+                .ToArray();
+            }
+            else
+            {
+                WeatherForecast[] a = {};
+                return a.ToArray();
+            }
+        }
+
+        [HttpGet(Name = "GetPosts")]
+        public  async  Task<string> Get()
+        {
+
+            HttpClient client = new HttpClient(); // Git Test App 
+           // Call asynchronous network methods in a try/catch block to handle exceptions.
+            try
+            {
+                using HttpResponseMessage response = await client.GetAsync("https://jsonplaceholder.typicode.com/posts");
+                response.EnsureSuccessStatusCode();
+                string responseBody = await response.Content.ReadAsStringAsync();
+                // Above three lines can be replaced with new helper method below
+                // string responseBody = await client.GetStringAsync(uri);
+                
+                return responseBody;
+            }
+            catch (HttpRequestException e)
+            {
+                return "";
+            }
+            
+            
+
+        }
+
+    }
+}
